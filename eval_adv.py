f857fabd0ebbfb9fa376e932b285bef29dab6480
Jiachen Sun <jiachens@oahu.eecs.umich.edu>
2021-05-04 14:52:40 -0400

    change attack

--- eval_adv.py
+++ eval_adv.py
@@ -3,7 +3,7 @@ Description:
 Autor: Jiachen Sun
 Date: 2021-03-29 21:31:47
 LastEditors: Jiachen Sun
-LastEditTime: 2021-04-23 17:36:28
+LastEditTime: 2021-05-04 14:52:26
 '''
 from __future__ import print_function
 import os
@@ -135,8 +135,10 @@ def adversarial(args,io,model=None, dataloader=None):
         #     adv_data = attack.random_drop(model,data,200)
         elif args.attack == 'add_50':
             adv_data = attack.pgd_adding_attack(model,data,label,50,eps=args.eps,alpha=args.alpha,iters=args.test_iter,repeat=1,mixup=False)
-        elif args.attack == 'add_100':
-            adv_data = attack.pgd_adding_attack(model,data,label,100,eps=args.eps,alpha=args.alpha,iters=args.test_iter,repeat=1,mixup=False)
+        elif args.attack == 'add_512':
+            adv_data = attack.pgd_adding_attack(model,data,label,512,eps=args.eps,alpha=args.alpha,iters=args.test_iter,repeat=1,mixup=False)
+        elif args.attack == 'add_156':
+            adv_data = attack.pgd_adding_attack(model,data,label,256,eps=args.eps,alpha=args.alpha,iters=args.test_iter,repeat=1,mixup=False)
         # elif args.attack == 'add_200':
         #     adv_data = attack.pgd_adding_attack(model,data,label,200,eps=args.eps,alpha=args.alpha,iters=args.test_iter,repeat=1,mixup=False)
         # elif args.attack == 'add_400':
